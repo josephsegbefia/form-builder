ecac6e0cfffe11d1babedc5bcb7fefd82e25e921
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-11 20:04:17 +0000

    Create FormFieldInput component

---
+++ src/components/FormFieldInput.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const FormFieldInput = ({label, type, fieldName, placeholder, helpText, required, handleFieldChange}) => {
+  const [value, setValue] = useState('');
+
+  const handleOnInputChange = e => {
+    setValue(e.target.value);
+
+    if(handleFieldChange) {
+      handleFieldChange(e.target.name, e.target.value)
+    }
+  }
+  return (
+    <div className = 'field'>
+      <label className = 'label'>{label}</label>
+      <div className = 'control'>
+        <input
+          className = 'input'
+          type = {type}
+          placeholder = {placeholder}
+          name = {fieldName}
+          onChange={handleOnInputChange}
+          required={required || false}
+        />
+      </div>
+      {
+        helpText &&
+        <p className = 'help'>{helpText}</p>
+      }
+    </div>
+  );
+};
+
+export default FormFieldInput;
